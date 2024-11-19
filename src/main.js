10f7363a1393c964b43db205ece638308b4a7555
Nataliia <nkopichinckaya@gmail.com>
2024-11-19 23:53:24 +0100

    add started pacege

---
+++ src/main.js
@@ -0,0 +1,10 @@
+import './js/header';
+import './js/hero';
+import './js/about-me';
+import './js/tech-skils';
+import './js/my-projects';
+import './js/benefits';
+import './js/faq';
+import './js/reviews';
+import './js/work-together';
+import './js/footer';
