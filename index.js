1cc43f96386fa789a2389b673c98079a8828a930
NataliiaKopychynska <nkopichinckaya@gmail.com>
2024-11-20 17:23:51 +0000

    Deploying to gh-pages from @ NataliiaKopychynska/portfolio-group-10@c89c3321487c30a0491901bdade3d255fa5178ff 🚀

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
-import"./assets/vendor-CsMCLY2v.js";(function(){const t=document.createElement("link").relList;if(t&&t.supports&&t.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))i(e);new MutationObserver(e=>{for(const r of e)if(r.type==="childList")for(const o of r.addedNodes)o.tagName==="LINK"&&o.rel==="modulepreload"&&i(o)}).observe(document,{childList:!0,subtree:!0});function s(e){const r={};return e.integrity&&(r.integrity=e.integrity),e.referrerPolicy&&(r.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?r.credentials="include":e.crossOrigin==="anonymous"?r.credentials="omit":r.credentials="same-origin",r}function i(e){if(e.ep)return;e.ep=!0;const r=s(e);fetch(e.href,r)}})();
+import"./assets/vendor-CsMCLY2v.js";(function(){const i=document.createElement("link").relList;if(i&&i.supports&&i.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))c(e);new MutationObserver(e=>{for(const t of e)if(t.type==="childList")for(const s of t.addedNodes)s.tagName==="LINK"&&s.rel==="modulepreload"&&c(s)}).observe(document,{childList:!0,subtree:!0});function o(e){const t={};return e.integrity&&(t.integrity=e.integrity),e.referrerPolicy&&(t.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?t.credentials="include":e.crossOrigin==="anonymous"?t.credentials="omit":t.credentials="same-origin",t}function c(e){if(e.ep)return;e.ep=!0;const t=o(e);fetch(e.href,t)}})();const n=document.querySelector(".review-list");fetch("https://portfolio-js.b.goit.study/api/reviews").then(r=>{if(!r.ok)throw new Error(r.status);return r.json()}).then(r=>{const i=r.map(o=>`
+  <li class="review-list-order" id = "${o._id}">
+            <p class="review-list-text">${o.review}</p>
+            <div class="review-list-box">
+            <img class="review-list-img" src="${o.avatar_url}" alt="${o.author}">
+                <p class="review-list-header">${o.author}</p>
+            </div>
+        </li>`).join("");n.insertAdjacentHTML("beforeend",i)}).catch(r=>console.log(r));
 //# sourceMappingURL=index.js.map
