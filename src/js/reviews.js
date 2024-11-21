23d858c451ebc2854b57bec08e9c55c6efa3f011
Roman Balakai <roman.balakay@gmail.com>
2024-11-21 13:31:33 +0200

    up

--- src/js/reviews.js
+++ src/js/reviews.js
@@ -8,18 +8,18 @@ navigation: {
     nextEl: '.swiper-button-n',
     prevEl: '.swiper-button-p',
   },
- 
  pagination: {
     el: '.swiper-pagination',
-  },   
+    },   
+ reachBeginning: '.swiper-button-p',
     keyboard: {
         enabled: true,
         // pageUpDown: true,
         onlyInViewport: true,
     },
     autoHeight: true,
-    height: 332,
-    swiperHeight: 332,
+    // height: 332,
+    // swiperHeight: 332,
     breakpoints: {
         1280: {
             slidesPerView: 2,
@@ -29,20 +29,42 @@ navigation: {
 }
 );
 
+swiper.on('reachEnd', function() {
+    var swjsNext = document.querySelector('.swiper-button-n');
+    swjsNext.style.border = '1px solid #e4e5e6';
+    swjsNext.style.pointerEvents = 'none';
+});
+
+swiper.on('reachBeginning', function() {
+    var swjsPrev = document.querySelector('.swiper-button-p');
+    swjsPrev.style.border = '1px solid #e4e5e6';
+    swjsPrev.style.pointerEvents = 'none';
+});
 
 
-const fetchReview = document.querySelector(".review-list")
-fetch("https://portfolio-js.b.goit.study/api/reviews")
-    .then((response) => {
-        if (!response.ok) {
-            throw new Error(response.status);
-        }
-        return response.json();
-    })
-    .then((reviews) => {
-        const markup = reviews.map((review) => {
-            return `
-  <li class="review-list-order swiper-slide" id = "${review._id}">
+swiper.on('fromEdge', function() {
+    var swjsNext = document.querySelector('.swiper-button-n');
+    swjsNext.style.display = 'flex';
+    swjsNext.style.border = '1px solid #292929';
+    swjsNext.style.pointerEvents = 'auto';
+    var swjsPrev = document.querySelector('.swiper-button-p');
+    swjsPrev.style.display = 'flex'; 
+    swjsPrev.style.border = '1px solid #292929';
+    swjsPrev.style.pointerEvents = 'auto';
+});
+
+const fetchReview = document.querySelector(".review-list");
+    fetch("https://portfolio-js.b.goit.study/api/reviews")
+        .then((response) => {
+            if (!response.ok) {
+                throw new Error(response.status);
+            }
+            return response.json();
+        })
+        .then((reviews) => {
+            const markup = reviews.map((review) => {
+                return `
+  <li class="review-list-order swiper-slide" id = "id_${review._id}">
   <div class="list-order-container">          
   <p class="review-list-text">${review.review}</p>
             <div class="review-list-box">
@@ -51,9 +73,8 @@ fetch("https://portfolio-js.b.goit.study/api/reviews")
                 </div>
             </div>
         </li>`;
+            }) 
+                .join("");
+            fetchReview.insertAdjacentHTML("beforeend", markup);
         })
-            .join("");
-        fetchReview.insertAdjacentHTML("beforeend", markup);
-    })
-    .catch((error) => console.log(error));
-
+        .catch((error) => console.log(error));
