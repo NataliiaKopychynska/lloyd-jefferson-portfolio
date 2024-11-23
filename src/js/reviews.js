7a94ee667051ce2049870c2c8c1cdac62468fe04
Roman Balakai <roman.balakay@gmail.com>
2024-11-23 19:36:00 +0200

    error

--- src/js/reviews.js
+++ src/js/reviews.js
@@ -1,7 +1,7 @@
 import Swiper from 'swiper/bundle';
-
 import 'swiper/css/bundle';
-
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
 
 const swiper = new Swiper('.swiper', {
 navigation: {
@@ -16,12 +16,9 @@ reachBeginning: '.swiper-button-p',
 reachEnd:'.swiper-button-n',
     keyboard: {
         enabled: true,
-        // pageUpDown: true,
         onlyInViewport: true,
     },
-    autoHeight: true,
-    // height: 332,
-    // swiperHeight: 332,
+    autoHeight: false,
     breakpoints: {
         1280: {
             slidesPerView: 2,
@@ -37,8 +34,6 @@ swiper.on('reachEnd', function() {
     swjsNext.style.pointerEvents = 'none';
     let btnIconN = document.querySelector('.btn-icon-n');
     btnIconN.style.stroke = '#e4e5e6';
-    // let swjsPrevH = document.querySelector('.swiper-button-n:hover');
-    // swjsPrevH.style.backgroundColor = '#f0f0f0';
 });
 
 swiper.on('reachBeginning', function() {
@@ -47,8 +42,6 @@ swiper.on('reachBeginning', function() {
     swjsPrev.style.pointerEvents = 'none';
     let btnIconP = document.querySelector('.btn-icon-p');
     btnIconP.style.stroke = '#e4e5e6';
-    // let swjsPrevP = document.querySelector('.swiper-button-p:hover');
-    // swjsPrevP.style.backgroundColor = '#f0f0f0';
 });
 
 swiper.on('fromEdge', function() {
@@ -58,40 +51,55 @@ swiper.on('fromEdge', function() {
     swjsNext.style.pointerEvents = 'auto';
     let btnIconN = document.querySelector('.btn-icon-n');
     btnIconN.style.stroke = '#292929';
-    //  let swjsPrevH = document.querySelector('.swiper-button-n:hover');
-    // swjsPrevH.style.backgroundColor = '#bbbbbb';
     let swjsPrev = document.querySelector('.swiper-button-p');
     swjsPrev.style.display = 'flex'; 
     swjsPrev.style.border = '1px solid #292929';
     swjsPrev.style.pointerEvents = 'auto';
     let btnIconP = document.querySelector('.btn-icon-p');
     btnIconP.style.stroke = '#292929';
-    //  let swjsPrevP = document.querySelector('.swiper-button-p:hover');
-    // swjsPrevP.style.backgroundColor = '#bbbbbb';
 });
 
+
 const fetchReview = document.querySelector(".review-list");
+const fetchNotReview = document.querySelector(".review-header");
     fetch("https://portfolio-js.b.goit.study/api/reviews")
         .then((response) => {
             if (!response.ok) {
-                throw new Error(response.status);
-            }
+                const notReviews = ["Not found"];
+                const markupNotReviews = notReviews
+                    .map((notReviev) => `
+                    <p class="review-not-found">${notReviev}</p>
+                `)
+                .join("");
+                fetchNotReview.insertAdjacentHTML("afterend", markupNotReviews);
+                throw new Error(response.status);  
+            } 
             return response.json();
         })
-        .then((reviews) => {
-            const markup = reviews.map((review) => {
+        .then((response) => {
+            const markup = response.map((review) => {
                 return `
-  <li class="review-list-order swiper-slide" id = "id_${review._id}">
-  <div class="list-order-container">          
-  <p class="review-list-text">${review.review}</p>
-            <div class="review-list-box">
-            <img class="review-list-img" src="${review.avatar_url}" alt="${review.author}">
+                <li class="review-list-order swiper-slide" id = "id_${review._id}">
+                <div class="list-order-container">          
+                <p class="review-list-text">${review.review}</p>
+                <div class="review-list-box">
+                <img class="review-list-img" src="${review.avatar_url}" alt="${review.author}">
                 <p class="review-list-header">${review.author}</p>
                 </div>
-            </div>
-        </li>`;
+                </div>
+                </li>`;
             }) 
                 .join("");
             fetchReview.insertAdjacentHTML("beforeend", markup);
         })
-        .catch((error) => console.log(error));
+        .catch(error => {
+             
+      iziToast.error({
+        title: 'Review not found',
+        message: `Error: ${error.message}`,
+          color: '#e74a3b',
+        //   target: '.swiper-arrow',
+        // position:  'bottomRight',
+      });
+         
+    });
