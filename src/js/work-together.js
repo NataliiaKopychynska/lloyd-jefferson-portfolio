b5f0a58835148ee0fbea4db19aadaac0b2d93d3d
Teslya Stepan <stepan199812@gmail.com>
2024-11-23 20:12:13 +0200

    work-together2.0

--- src/js/work-together.js
+++ src/js/work-together.js
@@ -1,49 +1,48 @@
 import axios from 'axios';
 
-document
-  .getElementById('contactForm')
-  .addEventListener('submit', async function (event) {
-    event.preventDefault();
+const togetherForm = document.querySelector('#contactForm');
+togetherForm.addEventListener('submit', async function (event) {
+  event.preventDefault();
 
-    axios.defaults.baseURL = 'https://portfolio-js.b.goit.study/api';
+  axios.defaults.baseURL = 'https://portfolio-js.b.goit.study/api';
 
-    const form = event.target;
-    const email = form.email.value.trim();
-    const message = form.message.value.trim();
+  const form = event.target;
+  const email = form.email.value.trim();
+  const message = form.message.value.trim();
 
-    if (!email || !message) {
-      showErrorMessage('Please fill in all the fields.');
-      return;
-    }
+  if (!email || !message) {
+    showErrorMessage('Please fill in all the fields.');
+    return;
+  }
 
-    try {
-      const response = await axios.post('/requests', {
-        email: email,
-        message: message,
-      });
-    } catch (error) {
-      console.error('Помилка:', error);
-      const errorMessage =
-        error.response?.data?.message ||
-        'An unexpected error occurred. Please try again.';
-      showErrorMessage(errorMessage);
-    }
-  });
+  try {
+    const response = await axios.post('/requests', {
+      email,
+      comment: message,
+    });
 
-function showModal(message) {
-  const modalText = document.querySelector('.together-modal-text');
-  modalText.textContent = message;
-  openModal();
-}
+    openModal();
+    form.reset();
+  } catch (error) {
+    console.error('Помилка:', error);
+    const errorMessage =
+      error.response?.data?.message ||
+      'An unexpected error occurred. Please try again.';
+    showErrorMessage(errorMessage);
+  }
+});
 
 function openModal() {
-  const backdrop = document.querySelector('.backdrop');
-  backdrop.classList.add('is-open');
+  const backdropTogether = document.querySelector('.backdrop-together');
+  backdropTogether.classList.add('are-open');
 }
 
+const modalClsBtn = document.querySelector('.modal-close-btn');
+modalClsBtn.addEventListener('click', closeModal);
+
 function closeModal() {
-  const backdrop = document.querySelector('.backdrop');
-  backdrop.classList.remove('is-open');
+  const backdropTogether = document.querySelector('.backdrop-together');
+  backdropTogether.classList.remove('are-open');
 }
 
 function showErrorMessage(message) {
